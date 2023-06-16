cbd3ef74674b833620535b0cf0b9947cc07efe7e
kleberlimagit <mov_kleber@hotmail.com>
2023-06-16 12:28:59 -0300

    overflow

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -5,18 +5,22 @@ import { FlagServiceService } from 'src/app/services/flag-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit{
-  public flags:DataCountry[] = []
-  
-  constructor(private flagService: FlagServiceService){
-  }
+export class HomeComponent implements OnInit {
+  public flags: DataCountry[] = [];
+
+  constructor(private flagService: FlagServiceService) {}
   ngOnInit(): void {
     this.flagService.findFlags().subscribe((data: DataCountry[]) => {
-      this.flags =  data;
-      console.log(this.flags)
-    });    
+      this.flags = data.sort((a: DataCountry, b: DataCountry) => {
+        if (a.name.common > b.name.common) {
+          return 1;
+        } else if (a.name.common < b.name.common) {
+          return -1;
+        } else return 0;
+      });
+      console.log(this.flags);
+    });
   }
-
 }

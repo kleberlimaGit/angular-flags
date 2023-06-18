662c10dbf3d46ad5f6f51a85674139c6b9492cfd
kleberlimagit <mov_kleber@hotmail.com>
2023-06-18 13:20:03 -0300

    adicionando mapa

--- src/app/components/pages/flag/flag.component.ts
+++ src/app/components/pages/flag/flag.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataCountry } from 'src/app/model/data-country';
 import { FlagService } from 'src/app/services/flag-service.service';
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-flag',
@@ -9,8 +11,10 @@ import { FlagService } from 'src/app/services/flag-service.service';
   styleUrls: ['./flag.component.scss']
 })
 export class FlagComponent implements OnInit{
-  formatNumber = new Intl.NumberFormat('pt-BR')
+  formatNumber = new Intl.NumberFormat('pt-BR');
+  faMapLocationDot = faMapLocationDot;
 
+  
   public flag: Partial<DataCountry> = {};
   constructor(private service: FlagService,  private activevedRouter: ActivatedRoute, private router: Router){}
   ngOnInit(): void {
@@ -20,7 +24,7 @@ export class FlagComponent implements OnInit{
   public findByFlagName(){
     const name = this.activevedRouter.snapshot.params['flagName']
     this.service.findFlagByName(name).subscribe({
-      next: (res:DataCountry[]) => {this.flag =  res[0]},
+      next: (res:DataCountry[]) => {this.flag =  res[0]; console.log(this.flag)},
       error: (res: Error) => this.router.navigate(['404'])
     })
 }
--- src/app/services/flag-service.service.ts
+++ src/app/services/flag-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DataCountry } from '../model/data-country';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
